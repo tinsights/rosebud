598b7b5a0c98469a057804e76788e06e51461e5b
mister T <aj.tinaes@icloud.com>
2023-04-19 16:44:12 +0800

    basic routing

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
 async function main() {
-  app.get("/", (req, res) => {
-    res.send("Hello World!");
+  app.use("/admin", require("./routes/adminRoutes"));
+  app.use("/", (req, res) => {
+    res.status(404).send("404 - Not Found");
   });
   app.listen(port, () => {
     console.log(`Rosebud at http://localhost:${port}`);
